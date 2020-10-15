29ac38078691a689a41f860ae4d27bd5d2dd1b4e
Sergio Isair <sergioisair@gmail.com>
2020-10-15 01:38:43 -0500

    Primer subida

--- main.go
+++ main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var n int
+	var x int
+	var suma int
+	fmt.Scan(&n)
+	var s []int
+	for i := 0; i < n; i++ {
+		fmt.Scan(&x)
+		s = append(s, x)
+	}
+	for i := 0; i < n; i++ {
+		suma = suma + s[i]
+	}
+	fmt.Print(suma)
 }
